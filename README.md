d44d348c1c7698dbba070b7b0ef663ca1a4f8d54
Remulo Costa <remulo.costa@gmail.com>
2020-03-26 00:02:05 -0300

    finalização back-end

---
+++ README.md
@@ -0,0 +1,14 @@
+<h1 align="center">
+  <img alt="BE THE HERO" title="BE THE HERO" src=".github/logo.png" width="200px" />
+</h1>
+
+# back-end semana OmniStack 11 - BE THE HERO
+
+Aplicação Back-end: API REST usando Node.js, Express, persistindo dados via Knex com banco de dados SQLite3, podendo ser configurado para outros como MSSQL, MySQL, PostgreSQL, Oracle.
+
+## dependencies:
+
+express,
+cors,
+Knex,
+sqlite3 
---
+++ src/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+/**
+ * Métodos HTTP:
+ *
+ * GET: Buscar/listar uma informação do back-end
+ * POST: Criar uma informação no back-end
+ * PUT: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+*/
+
+/**
+ * Tipo de parâmetros:
+ *
+ * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros, ordenação, paginação, ...)
+ * Route Params: Parâmetros utilizados para identificar recursos
+ * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+ */
+
+ /**
+  * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
+  * NoSQL: MongoDB, CouchDB, etc
+  */
+
+
+app.listen(3333);
